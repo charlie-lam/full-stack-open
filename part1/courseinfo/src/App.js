2c6ec2d09e0f81622533d5f774cdaf4430a62be9
charlie-lam <93406456+charlie-lam@users.noreply.github.com>
2023-03-26 19:22:53 +0100

    Part components added to Content component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -14,15 +14,9 @@ const Content = (props) => {
   const {part1, exercises1, part2, exercises2, part3, exercises3} = props
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   )
 }
