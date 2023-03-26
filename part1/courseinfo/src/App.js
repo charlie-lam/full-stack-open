50134eae26e2210ea64d294c26d5f3ea3253d682
charlie-lam <93406456+charlie-lam@users.noreply.github.com>
2023-03-26 18:40:47 +0100

    courseInfo part1 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,22 +1,3 @@
-const App = () => {
-  
-
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -24,6 +5,7 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  const {part1, exercises1, part2, exercises2, part3, exercises3} = props
   return (
     <div>
       <p>
@@ -36,6 +18,12 @@ const Content = (props) => {
         {part3} {exercises3}
       </p>
     </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
   )
 }
 
@@ -47,12 +35,13 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
 
   return (
     <div>
       <Header course={course} />
       <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total ... />
+      <Total total={total} />
     </div>
   )
 }
