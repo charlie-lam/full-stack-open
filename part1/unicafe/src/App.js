e2bc41824aba0b015060f0f8712cfed70238aa95
charlie-lam <93406456+charlie-lam@users.noreply.github.com>
2023-03-30 01:00:56 +0100

    1.6 add buttons to change state and add display elements

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +9,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h2>give feeback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
