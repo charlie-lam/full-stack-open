083a922fea4d1db15c35f2f00c2783a1261d4e68
charlie-lam <93406456+charlie-lam@users.noreply.github.com>
2023-04-20 00:35:09 +0100

    2.6 Form and display working

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,39 @@
 import { useState } from 'react'
 
+const Person = ({person}) => 
+  <p>
+    {person.name}
+  </p>
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setPersons([...persons, {name: newName}])
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange} value={newName}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <Person key={person.name} person={person}/>)}
     </div>
   )
 }
